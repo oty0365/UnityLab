062b611740c9970fd5e472177fac5ee9e847837f
ChanYoungOh <163986958+oty0365@users.noreply.github.com>
2026-02-09 23:14:36 +0900

    그래프 시각화

---
+++ Assets/OtyPackages/Pathfinder/Runtime/NodeData.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace OtyPackages.Pathfinder.Runtime
+{
+    public class NodeData
+    {
+        public bool canGo;
+        public Vector3 position;
+        
+        public float hCoast;
+        public float gCoast;
+        public float fCoast=>hCoast+gCoast;
+        
+        public NodeData parent;
+        
+        public NodeData(bool walkable, Vector3 worldPos) {
+            canGo = walkable;
+            position = worldPos;
+        }
+    }
+}
+
---
+++ Assets/OtyPackages/Pathfinder/Runtime/Pathfinder2D.cs
@@ -0,0 +1,8 @@
+namespace OtyPackages.Pathfinder.Runtime
+{
+    public class Pathfinder2D
+    {
+        
+    }
+}
+
